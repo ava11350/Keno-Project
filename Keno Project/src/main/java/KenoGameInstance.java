84c3525aa40b9ae5761f3d307767029fc0a25aa6
Chris Grams <chrisagrams@gmail.com>
2020-10-16 23:58:37 -0500

    Added checkWinnings

--- Keno Project/src/main/java/KenoGameInstance.java
+++ Keno Project/src/main/java/KenoGameInstance.java
@@ -4,11 +4,15 @@ import java.util.Random;
 public class KenoGameInstance {
     public ArrayList<Integer> userInput;
     public ArrayList<Integer> computerResult;
+    public ArrayList<Integer> hits;
     private int spotsToPlay;
+    public int moneyWon;
     public KenoGameInstance(int spots){
         userInput = new ArrayList<>();
         computerResult = new ArrayList<>();
+        hits = new ArrayList<>();
         spotsToPlay = spots;
+        moneyWon = 0;
     }
 
     public boolean pushInput(int val){ //push user input into userInput array. Returns true on success, false on error.
@@ -30,6 +34,60 @@ public class KenoGameInstance {
             temp = new Random().nextInt(80) + 1;
         }
         computerResult.add(temp);
+        if(userInput.contains(temp)){
+            System.out.println("Hit!");
+            hits.add(temp);
+            checkWinnings();
+        }
         return temp;
     }
+
+    public void checkWinnings(){
+        int size = hits.size();
+        switch (spotsToPlay){
+            case 1:
+                switch (size) {
+                    case 1:
+                        moneyWon = 2;
+                }
+            case 4:
+                switch (size){
+                    case 2:
+                        moneyWon = 1;;
+                    case 3:
+                        moneyWon = 5;
+                    case 4:
+                        moneyWon = 75;
+                }
+            case 8:
+                switch (size){
+                    case 4:
+                        moneyWon = 2;
+                    case 5:
+                        moneyWon = 12;
+                    case 6:
+                        moneyWon = 50;
+                    case 7:
+                        moneyWon = 750;
+                    case 8:
+                        moneyWon = 10000;
+                }
+            case 10:
+                switch (size){
+                    case 5:
+                        moneyWon = 2;
+                    case 6:
+                        moneyWon = 15;
+                    case 7:
+                        moneyWon = 150;
+                    case 8:
+                        moneyWon = 450;
+                    case 9:
+                        moneyWon = 4250;
+                    case 10:
+                        moneyWon = 100000;
+                }
+        }
+    }
+
 }
